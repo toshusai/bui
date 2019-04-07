5c60b68b8e24db833f461cee113438d49d135d63
toshusai <tyfltht17.hftkvtx18.klff9@gmail.com>
2019-04-07 11:22:38 +0900

    Update RectTransform Anchors system

--- component/recttransform.go
+++ component/recttransform.go
@@ -1,13 +1,64 @@
 package component
 
 import (
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/toshusai/bui"
 	"github.com/toshusai/bui/view"
 )
 
+type RectTransformMode int
+
+const (
+	Center RectTransformMode = iota
+)
+
+type Anchors struct {
+	Min bui.Vector2D
+	Max bui.Vector2D
+}
+
 type RectTransform struct {
-	Width  float32
-	Height float32
-	parent *view.Object
+	Position            mgl32.Vec3
+	Width               float32
+	Height              float32
+	parent              *view.Object
+	Anchos              Anchors
+	slippageX           float32
+	slippageY           float32
+	ParentRectTransform *RectTransform
+}
+
+func NewRectTransform() *RectTransform {
+	return &RectTransform{
+		Anchos: Anchors{
+			Min: bui.Vector2D{},
+			Max: bui.Vector2D{},
+		},
+	}
+}
+
+func (rt *RectTransform) GetAnchorsMinX() float32 {
+	return rt.ParentRectTransform.Width * rt.Anchos.Min.X
+}
+
+func (rt *RectTransform) GetAnchorsMaxX() float32 {
+	return rt.ParentRectTransform.Width * rt.Anchos.Max.X
+}
+
+func (rt *RectTransform) GetAnchorsMinY() float32 {
+	return rt.ParentRectTransform.Height * rt.Anchos.Min.Y
+}
+
+func (rt *RectTransform) GetAnchorsMaxY() float32 {
+	return rt.ParentRectTransform.Height * rt.Anchos.Max.Y
+}
+
+func (rt *RectTransform) IsPivotX() bool {
+	return rt.Anchos.Min.X == rt.Anchos.Max.X
+}
+
+func (rt *RectTransform) IsPivotY() bool {
+	return rt.Anchos.Min.Y == rt.Anchos.Max.Y
 }
 
 func (rt *RectTransform) SetParent(obj *view.Object) {
@@ -15,8 +66,11 @@ func (rt *RectTransform) SetParent(obj *view.Object) {
 }
 
 func (rt *RectTransform) Update() {
+
 }
 
 func (rt *RectTransform) Init() {
-
+	if rt.parent.Parent != nil {
+		rt.ParentRectTransform = rt.parent.Parent.GetComponent(&RectTransform{}).(*RectTransform)
+	}
 }
